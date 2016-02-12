c0382861aad41d589af084e6290f43c22964849c
Lujun Jian <ljian@smith.edu>
2016-02-12 00:03:50 -0500

    Compute PropShare

--- falconpropshare.py
+++ falconpropshare.py
@@ -53,20 +53,75 @@ class FalconPropShare(Peer):
         peers.sort(key=lambda p: p.id)
         # request all available pieces from all peers!
         # (up to self.max_requests from each)
+
+        # find the rarest pieces in the swarm
+        av_count_dict = dict()
         for peer in peers:
             av_set = set(peer.available_pieces)
+            for piece in av_set:
+                if piece not in av_count_dict.keys():
+                    av_count_dict[piece] = [1, [peer.id]]
+                else:
+                    av_count_dict[piece][0] += 1
+                    av_count_dict[piece][1].append(peer.id)
+        
+        for peer in peers:
+            # pieces that peer has
+            av_set = set(peer.available_pieces)
+            # intersection between what user needs and what other peers have
             isect = av_set.intersection(np_set)
-            n = min(self.max_requests, len(isect))
+            
+            # can send all the request in this round
+            if self.max_requests >= len(isect):
+                # write request message to right peers
+                for piece_id in isect:
+                    start_block = self.pieces[piece_id]
+                    r = Request(self.id, peer.id, piece_id, start_block)
+                    requests.append(r)
+                    
+            # pick rarest-first                    
+            else:
+                isect_list = []
+                # number of peers who have this piece and what piece
+                for isectPiece in isect:
+                    isect_list.append((av_count_dict[isectPiece][0],isectPiece))
+
+                # sort according to first index, which is # of peers who own it
+                isect_list.sort()
+                # the fewer peers have the piece, the rarer the piece is
+                # find the  # of people who own the rarest piece 
+                rarestCount = isect_list[0][0]
+                sameRareList = []
+                # find all equally rarest pieces
+                for el in isect_list:
+                    if el[0] == rarestCount:
+                        sameRareList.append(el[1])
+                # make sure the order is random        
+                random.shuffle(sameRareList)
+                
+                listSecond = []
+                # merge shuffled rarest list and the rest together
+                for p in isect_list[len(el):]:
+                    listSecond.append(p[1])
+                isectIDList = sameRareList + listSecond
+                # cut the list and get needed amount of peers'ID
+                isectIDList = isectIDList[:self.max_requests]
+                # write request message to right peers 
+                for piece_id in isectIDList:
+                    start_block = self.pieces[piece_id]
+                    r = Request(self.id, peer.id, piece_id, start_block)
+                    requests.append(r)
             # More symmetry breaking -- ask for random pieces.
             # This would be the place to try fancier piece-requesting strategies
             # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(isect, n):
+
+            #for piece_id in random.sample(isect, n):
                 # aha! The peer has this piece! Request it.
                 # which part of the piece do we need next?
                 # (must get the next-needed blocks in order)
-                start_block = self.pieces[piece_id]
-                r = Request(self.id, peer.id, piece_id, start_block)
-                requests.append(r)
+                #start_block = self.pieces[piece_id]
+                #r = Request(self.id, peer.id, piece_id, start_block)
+                #requests.append(r)
 
         return requests
 
@@ -89,22 +144,67 @@ class FalconPropShare(Peer):
         # has a list of Download objects for each Download to this peer in
         # the previous round.
 
+        bwRateOpti = 0.1
+        uploads = []
+        if round != 0:
+            # collect the history of download
+            prevDownHistory = history.downloads[round-1]
+            historyDict = dict()
+            
+            #go through the history of round - 1
+            for downLoad in prevDownHistory:
+                fromId = downLoad.from_id
+                if fromId not in historyDict.keys():
+                    historyDict[fromId] = downLoad.blocks
+                else:
+                    historyDict[fromId] += downLoad.blocks                  
+
         if len(requests) == 0:
             logging.debug("No one wants my pieces!")
             chosen = []
             bws = []
         else:
-            logging.debug("Still here: uploading to a random peer")
+            #logging.debug("Still here: uploading to a random peer")
             # change my internal state for no reason
             self.dummy_state["cake"] = "pie"
 
-            request = random.choice(requests)
-            chosen = [request.requester_id]
-            # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
+            # get requester List
+            requesterList = []
+            for request in requests:
+                if request.requester_id not in requesterList:
+                    requesterList.append(request.requester_id)
 
-        # create actual uploads out of the list of peer ids and bandwidths
-        uploads = [Upload(self.id, peer_id, bw)
-                   for (peer_id, bw) in zip(chosen, bws)]
+            # dictionary for who to upload to
+            unchokingDict = dict()
+            for requester in requesterList:
+                if requester in historyDict.keys():
+                    unchokingDict[requester] = historyDict[requester]
+
+            totalBlocks = 0
+            for peer in unchokingDict.keys():
+                totalBlocks += unchokingDict[peer]
+
+            for peer in unchokingDict.keys():
+                percentage = (unchokingDict[peer]/totalBlocks)*(1-bwRateOpti)
+                unchokingDict[peer]= percentage
+                
+
+            # leave candidate for optimistic unchoking in requests
+            for request in requests:
+                    if request.requester_id in unchokingDict.keys():
+                        requests.remove(request)
+
+            uploads = []
+            for peer in unchokingDict.keys():
+                percentage = unchokingDict[peer]
+                uploads.append(Upload(self.id, peer,
+                                      int(self.up_bw*percentage)))
+
+            if len(requests) != 0:
+                luckyRequest = random.choice(requests)
+                requesterId = luckyRequest.requester_id
+                bwForOpti = self.up_bw*bwRateOpti
+
+                uploads.append(Upload(self.id, requesterId, int(bwForOpti)))               
             
         return uploads
